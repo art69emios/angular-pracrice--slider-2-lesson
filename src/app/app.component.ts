dcd989e62ef89e7afd5b161aa6f2753a4e60e143
Artem <artemios69soprun@gmail.com>
2022-12-09 17:47:59 +0000

    slader with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'homework3';
+  selectImg:string  = "./assets/3 (1).png"
+  imgArray:Array<string> = ["./assets/3 (1).png","./assets/3 (2).png","./assets/3 (3).png"]
+  i  = 0
+ 
+
+
+   increment (){
+    this.selectImg = this.imgArray[this.i]
+    this.i++
+
+    if(this.i == this.imgArray.length){
+      this.i = 0
+    }
+} 
+dicrement (){
+  this.selectImg = this.imgArray[this.i]
+  this.i--
+  if(this.i < 0){
+    this.i = 2
+  }
+} 
+ 
+
+  
+
+
 }
